6f62514ee02138a855aed1812d8047d6deb17e41
Will Epperson <willepp@live.com>
2022-04-22 13:39:00 -0400

    update model too for reactivity

--- src/ProfileModel.ts
+++ src/ProfileModel.ts
@@ -17,8 +17,11 @@ import type {
     INomChartData
 } from "./dataAPI/exchangeInterfaces"
 
+type ExecResult = { "content": string[], "et": number }
+
 
 export class ProfileModel { // implements Executor 
+    private exec_count = 0;
 
     private _sessionContext: ISessionContext;
     private _notebook: NotebookAPI;
@@ -71,6 +74,7 @@ export class ProfileModel { // implements Executor
 
     public async updateRootData() {
         let alldf = await this.getAllDataFrames()
+        console.log("[STORE] updating dataFramesAndCols to ", alldf)
         dataFramesAndCols.set(alldf)
     }
 
@@ -79,8 +83,9 @@ export class ProfileModel { // implements Executor
 
     private sendCodeToKernel(
         code: string,
-        onReply?: (type: string, content: any) => void,
-        onDone?: (arg_0?: string) => void
+        exec_time: number,
+        onReply?: (type: string, content: any, et: number) => void,
+        onDone?: (arg_0?: any) => void
     ) {
         // await this.ready
         let future = this.session.session?.kernel?.requestExecute({
@@ -91,15 +96,13 @@ export class ProfileModel { // implements Executor
         // this is the output of the execution, may return things multiple times as code runs
         future.onIOPub = (msg: KernelMessage.IIOPubMessage) => {
             var msg_type = msg.header.msg_type;
-            switch (msg_type) {
-                case 'execute_result':
-                case 'display_data':
-                case 'update_display_data':
-                case 'stream':
-                    if (onReply) onReply(msg_type + '', msg.content);
-                    break;
-                default:
-                    break;
+
+            if ((msg_type === 'execute_result')
+                || (msg_type === 'display_data')
+                || (msg_type === 'update_display_data')
+                || (msg_type === 'stream')
+            ) {
+                if (onReply) onReply(msg_type + '', msg.content, exec_time);
             }
 
         };
@@ -107,7 +110,8 @@ export class ProfileModel { // implements Executor
         // when execution is done
         future.done.then(
             reply => {
-                if (onDone) onDone(reply.content.status);
+                // if (onDone) onDone(reply.content.status);
+                if (onDone) onDone(reply);
             },
             error => {
                 console.error('Code run failed: ', error);
@@ -116,33 +120,38 @@ export class ProfileModel { // implements Executor
         );
     }
 
-    private async executeCode(code: string = ''): Promise<string[]> {
+    private async executeCode(code: string = '', exec_time: number): Promise<ExecResult> {
         // await this.ready;
 
-        return new Promise<string[]>(resolve => {
-            let onReply = (type: string, content: any) => {
-                console.log("code executed: ", code, "\nReturned as ", type, "with content: ", content)
+        return new Promise<ExecResult>(resolve => {
+            let onReply = (type: string, content: any, et: number) => {
+                // console.log("code executed: ", code, "\nReturned as ", type, "with content: ", content)
 
-                switch (type) {
-                    case 'execute_result':
-                    case 'display_data':
-                    case 'update_display_data':
-                        // These three all return same content except execute result has the execution count
-                        // TODO unclear how to get data back in JSON?
-                        let response: string[] = content.data['text/plain'].split('\n');
-                        resolve(response);
-                        break;
-                    case 'stream':
-                        let response3: string[] = content.text.split('\n');
-                        response3.pop();
-                        resolve(response3);
-                        break;
-                    default:
-                        break;
+                console.log(`[${type} @${et}]: Code executed: ${code} returned: `, content)
+
+                if ((type === 'execute_result') || (type === 'display_data') || (type === 'update_display_data')) {
+                    let cont: string[] = content.data['text/plain'].split('\n');
+                    let response = { "content": cont, "et": et }
+                    // console.log("Handling execute content, resolving...", response)
+                    resolve(response);
+                } else if (type === "stream") {
+                    let cont = content.text.split('\n');
+                    cont.pop();
+
+                    let response = { "content": cont, "et": et }
+                    // console.log("Handling stream content, resolving...", response)
+                    resolve(response);
+                } else {
+                    console.log("Unhandled message type: ", type, "with content ", content)
                 }
             };
 
-            this.sendCodeToKernel(code, onReply);
+            // let onDone = (content: any) => {
+            //     console.log("On done for code: ", code, "has content...", content)
+            // }
+
+            // this.sendCodeToKernel(code, exec_time, onReply, onDone);
+            this.sendCodeToKernel(code, exec_time, onReply);
         });
     }
 
@@ -152,6 +161,8 @@ export class ProfileModel { // implements Executor
     public async getAllDataFrames(): Promise<IDFColMap> {
         await this.ready;
         let var_names = await this.getVariableNames();
+
+        console.log("Var names are...", var_names)
 
         if (var_names) {
             let isDF = await this.getDFVars(var_names);
@@ -189,9 +200,11 @@ export class ProfileModel { // implements Executor
 
     public async getVariableNames(): Promise<string[]> {
         let code = '%who_ls'; // a python magic command
+        let et = this.exec_count
+        this.exec_count++;
 
         return new Promise<string[]>(resolve => {
-            let onReply = (type: string, content: any) => {
+            let onReply = (type: string, content: any, exec_t: number) => {
                 if (type == 'execute_result') {
                     // parse data into usable format
                     let data = (content.data['text/plain'] + '').replace(/'/g, '"');
@@ -199,11 +212,12 @@ export class ProfileModel { // implements Executor
                     let names = JSON.parse(jsn).names;
 
                     // return variable names
+                    console.log("getVariableNames sent at ", et, " returned at ", exec_t)
                     resolve(names);
                 }
             };
 
-            this.sendCodeToKernel(code, onReply);
+            this.sendCodeToKernel(code, et, onReply);
         });
     }
 
@@ -213,7 +227,16 @@ export class ProfileModel { // implements Executor
         */
         let code_lines = ['import pandas as pd']; // TODO better way to make sure pandas in env?
         varNames.forEach(name => code_lines.push(`print(type(${name}) == pd.DataFrame)`))
-        return this.executeCode(code_lines.join('\n'));
+
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code_lines.join('\n'), et);
+        console.log("Result in getDFVars: ", res)
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code_lines} time sent = ${et}, time back = ${returned_time}`)
+
+        return content
     }
 
     private async getColumns(varName: string): Promise<string[]> {
@@ -222,13 +245,28 @@ export class ProfileModel { // implements Executor
         Returns array of "True" or "False" if that variable is a pandas dataframe
         */
         let code = `print(${varName}.dtypes)`;
-        return this.executeCode(code);
+        let et = this.exec_count
+        this.exec_count++;
+
+        let res = await this.executeCode(code, et);
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code} time sent = ${et}, time back = ${returned_time}`)
+
+        return content
     }
 
-    public async getShape(dfName: string): Promise<number[]> {
+    public async getShape(dfName: string, colInfo?: IColTypeTuple[]): Promise<number[]> {
         let code = `print(${dfName}.shape)`; // returns '(3, 2)' so need to parse
-        let shapeStringArr = await this.executeCode(code);
-        let shapeString = shapeStringArr[0];
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code, et);
+
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code} time sent = ${et}, time back = ${returned_time}`)
+
+        let shapeString = content[0];
 
         return shapeString
             .substring(1, shapeString.length - 1)
@@ -242,9 +280,15 @@ export class ProfileModel { // implements Executor
         */
 
         const code = `print(${dfName}["${colName}"].head(${n}))`;
-        let res = await this.executeCode(code);
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code, et);
 
-        return res.slice(0, -1).map(x => x.split(/\s+/)[1])
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code} time sent = ${et}, time back = ${returned_time}`)
+
+        return content.slice(0, -1).map(x => x.split(/\s+/)[1])
     }
 
     public async getQuantMeta(dfName: string, colName: string): Promise<IQuantMeta> {
@@ -252,18 +296,30 @@ export class ProfileModel { // implements Executor
         let median_code = `print(${dfName}["${colName}"].median())`
         let numNull_code = `print(${dfName}["${colName}"].isna().sum())`
         let code_lines = [mean_code, median_code, numNull_code]
-        let res = await this.executeCode(code_lines.join('\n'));
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code_lines.join('\n'), et);
+
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code_lines} time sent = ${et}, time back = ${returned_time}`)
         // return new Promise(resolve => resolve({ "mean": res[0], "median": res[1], "num_invalid": res[2] }))
-        return { "mean": res[0], "median": res[1], "num_invalid": res[2] }
+        return { "mean": content[0], "median": content[1], "num_invalid": content[2] }
     }
 
     public async getNomMeta(dfName: string, colName: string): Promise<INomMeta> {
         let numUnique_code = `print(${dfName}["${colName}"].nunique())`
         let numNull_code = `print(${dfName}["${colName}"].isna().sum())`
         let code_lines = [numUnique_code, numNull_code]
-        let res = await this.executeCode(code_lines.join('\n'));
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code_lines.join('\n'), et);
+
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code_lines} time sent = ${et}, time back = ${returned_time}`)
         // return new Promise(resolve => resolve({ "unique": res[0], "num_invalid": res[1] }))
-        return { "num_unique": res[0], "num_invalid": res[1] }
+        return { "num_unique": content[0], "num_invalid": content[1] }
     }
 
 
@@ -274,8 +330,14 @@ export class ProfileModel { // implements Executor
         */
 
         let code = `print(${dfName}["${colName}"].value_counts()[:${n}].to_json())`
-        let res = await this.executeCode(code);
-        let json_res = JSON.parse(res[0].replace(/'/g, "")) // remove single quotes bc not JSON parseable
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code, et);
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code} time sent = ${et}, time back = ${returned_time}`)
+
+        let json_res = JSON.parse(content[0].replace(/'/g, "")) // remove single quotes bc not JSON parseable
         let data: INomChartData = []
         Object.keys(json_res).forEach(k => {
             data.push({ [colName]: k, "count": json_res[k] })
@@ -291,8 +353,14 @@ export class ProfileModel { // implements Executor
         */
 
         let code = `print(${dfName}["${colName}"].value_counts(bins=min(${maxbins}, ${dfName}["${colName}"].nunique()), sort=False).to_json())`
-        let res = await this.executeCode(code);
-        let json_res = JSON.parse(res[0].replace(/'/g, "")) // remove single quotes bc not JSON parseable
+        let et = this.exec_count
+        this.exec_count++;
+        let res = await this.executeCode(code, et);
+        let content = res["content"]
+        let returned_time = res["et"]
+        console.log(`${code} time sent = ${et}, time back = ${returned_time}`)
+
+        let json_res = JSON.parse(content[0].replace(/'/g, "")) // remove single quotes bc not JSON parseable
         let data: any[] = []
 
         Object.keys(json_res).forEach(k => {
