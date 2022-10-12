8736318677cbe9a4115a08cccbdacc6a2c031eaa
Will Epperson <willepp@live.com>
2022-10-12 17:07:13 -0400

    include zoom number

--- src/components/viz/timestamp/TimestampProfileSummary.svelte
+++ src/components/viz/timestamp/TimestampProfileSummary.svelte
@@ -4,11 +4,17 @@
      * This component provides summary information about the
      * timestamp profile at the top of the detail plot.
      */
-    import { intervalToTimestring } from '../../utils/formatters';
+    import {
+        intervalToTimestring,
+        formatBigNumberPercentage
+    } from '../../utils/formatters';
 
     import type { Interval } from '../../../common/exchangeInterfaces';
 
     export let interval: Interval;
+    export let numTotalRows: number;
+    export let numZoomedRows: number;
+    export let zoomed = false;
 </script>
 
 <div
@@ -18,10 +24,20 @@
         grid-template-columns: auto;
     "
 >
-    <div>
-        {#if interval}
-            Range is {intervalToTimestring(interval)}
-        {/if}
+    <div class="flex">
+        <div class="grow">
+            {#if interval}
+                Range is {intervalToTimestring(interval)}
+            {/if}
+        </div>
+        <div class="text-right">
+            {#if zoomed}
+                Zoomed to {numZoomedRows} rows ({formatBigNumberPercentage(
+                    numZoomedRows / numTotalRows
+                )}).
+            {/if}
+        </div>
     </div>
+
     <div class="text-gray-400">Ctrl + Click + Drag to zoom</div>
 </div>
