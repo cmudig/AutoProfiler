8162671d00ebc097988e1300dcb4451a3b0222b5
Will Epperson <willepp@live.com>
2022-10-11 21:14:54 -0400

    Tooltips (#52)
    * add tooltip component
    
    * add tooltips for type and null

---
+++ src/components/tooltip/TooltipContent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let maxWidth: string = undefined;
+</script>
+
+<div
+  class="bg-gray-700 text-white rounded p-2 pt-1 pb-1"
+  style:max-width={maxWidth}
+>
+  <slot />
+</div>
