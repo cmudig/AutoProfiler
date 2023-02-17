83e2d5875442d624369e6bd395e6f67cd6905408
Will Epperson <willepp@live.com>
2023-02-16 19:12:43 -0500

    use expander menu for summary stats

--- src/components/fact-panel/VizOrStats.svelte
+++ src/components/fact-panel/VizOrStats.svelte
@@ -15,85 +15,91 @@
         DATA_TYPE_COLORS,
         BOOLEANS
     } from '../data-types/pandas-data-types';
+    import CollapsibleCard from '../nav/CollapsibleCard.svelte';
+    import ExpanderButton from '../nav/ExpanderButton.svelte';
 
     export let dfName: string;
     export let colName: string;
     export let totalRows: number;
     export let type: string;
-    export let vizToggleOption: string;
     export let summary: ColumnSummary;
     export let isIndex = false;
     export let wrapperDivWidth: number;
     export let nullCount: number;
+
+    let expanded = false;
+
+    type ValidChartType = 'quant' | 'cat' | 'temporal';
+
+    function determineValidChartType(
+        t: string,
+        s: ColumnSummary
+    ): ValidChartType {
+        if (NUMERICS.has(t) && s?.quantMeta && s?.histogram?.length) {
+            return 'quant';
+        } else if ((CATEGORICALS.has(t) || BOOLEANS.has(t)) && s?.topK) {
+            return 'cat';
+        } else if (TIMESTAMPS.has(type) && summary?.histogram?.length) {
+            return 'temporal';
+        }
+
+        return undefined;
+    }
+
+    $: validatedChartType = determineValidChartType(type, summary);
+
+    console.log('validatedChartType is', validatedChartType);
 </script>
 
 <div>
-    <div class="flex gap-1 items-center pb-1">
-        <div class="justify-end">
-            <select
-                class="rounded border border-6 bg-gray-100 hover:border-gray-300"
-                bind:value={vizToggleOption}
-            >
-                <option value={'viz'}>Chart</option>
-                <option value={'text'}>Summary</option>
-            </select>
-        </div>
+    <div>
+        {#if validatedChartType === 'quant'}
+            <NumericHistogram
+                {dfName}
+                {colName}
+                {type}
+                {isIndex}
+                width={wrapperDivWidth}
+                height={65}
+                data={summary.histogram}
+                min={summary.quantMeta.min}
+                qlow={summary.quantMeta.q25}
+                median={summary.quantMeta.q50}
+                qhigh={summary.quantMeta.q75}
+                mean={summary.quantMeta.mean}
+                max={summary.quantMeta.max}
+            />
+        {:else if validatedChartType === 'cat'}
+            <TopKSummary
+                color={DATA_TYPE_COLORS[type].bgClass}
+                {totalRows}
+                topK={summary.topK}
+            />
+        {:else if validatedChartType === 'temporal'}
+            <TimestampDetail
+                data={convertToTimeBin(summary?.histogram)}
+                xAccessor="ts_end"
+                yAccessor="count"
+                height={160}
+                width={wrapperDivWidth}
+                interval={summary?.timeInterval}
+            />
+        {/if}
     </div>
-    {#if vizToggleOption === 'viz'}
-        <div>
-            {#if NUMERICS.has(type) && summary?.quantMeta && summary?.histogram?.length}
-                <NumericHistogram
-                    {dfName}
-                    {colName}
-                    {type}
-                    {isIndex}
-                    width={wrapperDivWidth}
-                    height={65}
-                    data={summary.histogram}
-                    min={summary.quantMeta.min}
-                    qlow={summary.quantMeta.q25}
-                    median={summary.quantMeta.q50}
-                    qhigh={summary.quantMeta.q75}
-                    mean={summary.quantMeta.mean}
-                    max={summary.quantMeta.max}
-                />
-                <div class="mt-1">
+
+    <div class="pt-2">
+        <CollapsibleCard bind:open={expanded}>
+            <div slot="header" class="dfprofile-header flex gap-1 items-center">
+                <ExpanderButton rotated={expanded} />
+                <p>
+                    {#if expanded}Hide {:else}Show{/if} summary
+                </p>
+            </div>
+
+            <div slot="header-no-collapse">
+                {#if validatedChartType}
                     <ExportChartButton
-                        chartType={'quant'}
-                        {dfName}
-                        {colName}
-                        exportOptions={{
-                            numBins: summary.histogram.length
-                        }}
-                        {isIndex}
-                    />
-                </div>
-            {:else if (CATEGORICALS.has(type) || BOOLEANS.has(type)) && summary?.topK}
-                <TopKSummary
-                    color={DATA_TYPE_COLORS[type].bgClass}
-                    {totalRows}
-                    topK={summary.topK}
-                />
-                <div class="mt-1">
-                    <ExportChartButton
-                        chartType={'cat'}
-                        {dfName}
-                        {colName}
-                        {isIndex}
-                    />
-                </div>
-            {:else if TIMESTAMPS.has(type) && summary?.histogram?.length}
-                <TimestampDetail
-                    data={convertToTimeBin(summary?.histogram)}
-                    xAccessor="ts_end"
-                    yAccessor="count"
-                    height={160}
-                    width={wrapperDivWidth}
-                    interval={summary?.timeInterval}
-                />
-                <div class="mt-1">
-                    <ExportChartButton
-                        chartType={'temporal'}
+                        chartType={validatedChartType}
                         {dfName}
                         {colName}
                         exportOptions={{
@@ -101,30 +107,32 @@
                         }}
                         {isIndex}
                     />
-                </div>
-            {/if}
-        </div>
-    {:else}
-        <div class="pr-2">
-            {#if NUMERICS.has(type)}
-                <NumericalStats
-                    {dfName}
-                    {colName}
-                    {isIndex}
-                    stats={summary.quantMeta}
-                />
-            {:else if CATEGORICALS.has(type)}
-                <StringStats
-                    {dfName}
-                    {colName}
-                    {isIndex}
-                    stats={summary.stringMeta}
-                    unique={summary.cardinality}
-                    rows={totalRows - nullCount}
-                />
-            {:else if TIMESTAMPS.has(type)}
-                <TempFacts facts={summary.temporalMeta} />
-            {/if}
-        </div>
-    {/if}
+                {/if}
+            </div>
+
+            <div slot="body" class="pl-5">
+                {#if validatedChartType === 'quant'}
+                    <NumericalStats
+                        {dfName}
+                        {colName}
+                        {isIndex}
+                        stats={summary.quantMeta}
+                    />
+                {:else if CATEGORICALS.has(type)}
+                    <StringStats
+                        {dfName}
+                        {colName}
+                        {isIndex}
+                        stats={summary.stringMeta}
+                        unique={summary.cardinality}
+                        rows={totalRows - nullCount}
+                    />
+                {:else if validatedChartType === 'temporal'}
+                    <TempFacts facts={summary.temporalMeta} />
+                {:else}
+                    <p class="text-gray-600">No summary available.</p>
+                {/if}
+            </div>
+        </CollapsibleCard>
+    </div>
 </div>
