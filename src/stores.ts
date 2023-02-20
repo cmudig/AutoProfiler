ee88ad9f3e5df9c6cc5f4dee2dafe3661974c972
Will Epperson <willepp@live.com>
2023-02-20 12:23:14 -0500

    set logging true by default

--- src/stores.ts
+++ src/stores.ts
@@ -2,5 +2,5 @@ import { type Writable, writable } from 'svelte/store';
 
 // UI stores
 export const currentHoveredCol: Writable<string> = writable(undefined);
-export const allowLogs: Writable<boolean> = writable(false);
+export const allowLogs: Writable<boolean> = writable(true);
 export const showIndex: Writable<boolean> = writable(false);
