c0c307574a81c0bc941f8fc45f683510bff8f210
Will Epperson <willepp@live.com>
2023-04-20 19:27:29 -0400

    Change logs to cumulative and monitor data updates

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -10,9 +10,10 @@ interface LogEvent {
 }
 
 interface AggregatedLog {
-    uniqueDfs: number;
-    uniqueCols: number;
-    reexaminedDfs: number;
+    totalDfToggles: number;
+    totalColInteractions: number;
+    uniqueWUpdatesDfs: number;
+    uniqueWUpdatesCols: number;
     selectionExports: number;
     chartExports: number;
     summaryfactExports: number;
@@ -26,21 +27,43 @@ interface AggregatedLog {
     sessionIdx: number;
 }
 
-// Log every 5 min
 // minutes * (seconds * milliseconds)
 const LOG_INTERVAL = 10 * (60 * 1000)
+// const LOG_INTERVAL = (10 * 1000)
 
+/**
+ * Keeps track of cumulative logs across a session. 
+ * A session starts when the extension is loaded or browser is refreshed.
+ */
 export class Logger {
+    // tracking vars
     private _logs: LogEvent[] = []
     private _lastOpenedStatus: "open" | "closed";
     private _lastLoggedTime: Date;
+    private sessionStartTime: Date;
     private sessionID: string
     private sessionIdx: number = 0
+    private colCache = new Map<string, Set<string>>()
+
+    // cumulative stats
+    private totalDfToggles = 0
+    private totalColInteractions = 0
+    private numDfsWUpdate = 0
+    private numColsWUpdate = 0
+    private selectionExports = 0
+    private chartExports = 0
+    private summaryfactExports = 0
+    private totalDataUpdates = 0
+    private totalOpens = 0
+    private totalCloses = 0
+    private timeOpen = 0
+
     constructor() {
         this._logs = []
         this._lastOpenedStatus = "closed"
         this._lastLoggedTime = new Date()
         this.sessionID = guidGenerator() // regenerated when extension reloads on page refresh or new tab
+        this.sessionStartTime = new Date()
         setInterval(() => {
             this.save()
         }, LOG_INTERVAL)
@@ -78,83 +101,78 @@ export class Logger {
      * Aggregate total number of exports as well
      */
     private aggregateLogs(endTime: Date): AggregatedLog {
-        // statistics
-        let uniqueDfs = new Set()
-        let uniqueCols = new Set()
-        let selectionExports = 0
-        let chartExports = 0
-        let summaryfactExports = 0
-        let totalDataUpdates = 0
-        let totalOpens = 0
-        let totalCloses = 0
-        let totalTimeInterval = endTime.getTime() - this._lastLoggedTime.getTime()
-        let timeOpen = 0
         let lastToggleTime = this._lastLoggedTime
-
-        // reexaimned dfs for when looked at a dataframe after an update
-        let updatedDfs = new Set()
-        let reexaminedDfs = 0
 
         this._logs.forEach((log: LogEvent) => {
             if ((log.eventname === "ToggleDFOpen") || (log.eventname === "ToggleDFClose")) {
-                if (updatedDfs.has(log.details.dfName)) {
-                    reexaminedDfs += 1
-                    updatedDfs.delete(log.details.dfName) // delete updated dfs after looked at again
+                if (!this.colCache.has(log.details.dfName)) {
+                    this.numDfsWUpdate += 1
+                    let s = new Set<string>()
+                    this.colCache.set(log.details.dfName, s)
                 }
-                uniqueDfs.add(log.details.dfName)
+                this.totalDfToggles += 1
             } else if (log.eventname === "ColumnToggleOpen" || log.eventname === "ColumnToggleClose" || log.eventname === "ColumnHover") {
-                if (updatedDfs.has(log.details.dfName)) {
-                    reexaminedDfs += 1
-                    updatedDfs.delete(log.details.dfName)
+                if (this.colCache.has(log.details.dfName)) {
+                    let cache = this.colCache.get(log.details.dfName)
+                    if (!cache.has(log.details.colName)) {
+                        this.numColsWUpdate += 1
+                        cache.add(log.details.colName)
+                    }
+                } else {
+                    let uniqueCols = new Set<string>()
+                    uniqueCols.add(log.details.colName)
+                    this.colCache.set(log.details.dfName, uniqueCols)
+                    this.numColsWUpdate += 1
+                    this.numDfsWUpdate += 1
                 }
-                uniqueDfs.add(log.details?.dfName)
-                uniqueCols.add(`${log.details?.dfName}.${log.details?.colName}`)
+                this.totalColInteractions += 1
             } else if (log.eventname === "export") {
                 if (log.details?.exportType === "selection") {
-                    selectionExports += 1
+                    this.selectionExports += 1
                 } else if (log.details?.exportType === "chart") {
-                    chartExports += 1
+                    this.chartExports += 1
                 } else if (log.details?.exportType === "summaryfact") {
-                    summaryfactExports += 1
+                    this.summaryfactExports += 1
                 }
             } else if (log.eventname === "AutoProfiler.toggleOpen") {
-                totalOpens += 1
-
+                this.totalOpens += 1
                 this._lastOpenedStatus = "open"
                 lastToggleTime = log.timestamp
             } else if (log.eventname === "AutoProfiler.toggleClosed") {
-                totalCloses += 1
+                this.totalCloses += 1
                 let delta = log.timestamp.getTime() - lastToggleTime.getTime()
-                timeOpen += delta
+                this.timeOpen += delta
 
                 this._lastOpenedStatus = "closed"
                 lastToggleTime = log.timestamp
             } else if (log.eventname === "AutoProfiler.updateData") {
-                totalDataUpdates += 1
-                updatedDfs.add(log.details?.dfName)
+                this.totalDataUpdates += 1
+                this.colCache.delete(log.details?.dfName)
             }
         })
 
         // if the last event was an open, we need to add the time since the last event to the open time
         let finalDelta = endTime.getTime() - lastToggleTime.getTime()
         if (this._lastOpenedStatus === "open") {
-            timeOpen += finalDelta
+            this.timeOpen += finalDelta
         }
 
-        let timeOpenPercent = (timeOpen / totalTimeInterval) * 100
+        const totalTimeInterval = endTime.getTime() - this.sessionStartTime.getTime()
+        let timeOpenPercent = (this.timeOpen / totalTimeInterval) * 100
 
         return {
-            uniqueDfs: uniqueDfs.size,
-            uniqueCols: uniqueCols.size,
-            reexaminedDfs,
-            selectionExports,
-            chartExports,
-            summaryfactExports,
-            totalDataUpdates,
-            totalOpens,
-            totalCloses,
+            totalDfToggles: this.totalDfToggles,
+            totalColInteractions: this.totalColInteractions,
+            uniqueWUpdatesDfs: this.numDfsWUpdate,
+            uniqueWUpdatesCols: this.numColsWUpdate,
+            selectionExports: this.selectionExports,
+            chartExports: this.chartExports,
+            summaryfactExports: this.summaryfactExports,
+            totalDataUpdates: this.totalDataUpdates,
+            totalOpens: this.totalOpens,
+            totalCloses: this.totalCloses,
             timeOpenPercent,
-            startTime: this._lastLoggedTime,
+            startTime: this.sessionStartTime,
             endTime,
             sessionID: this.sessionID,
             sessionIdx: this.sessionIdx
